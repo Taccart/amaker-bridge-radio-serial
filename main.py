21df4f908bd1c136c2df7670ff9013505720e5b7
TA aMaker <thierry@accart.name>
2025-06-15 09:53:49 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
+def broadcast_bridge_info():
+    
+    msg = control.device_name() +"_"+str(control.device_serial_number())+" is Radio-Serial bridge listening on "+str(radio_group)
+    print(msg)
+    radio.send_string(msg )
+    serial.write_string(msg)
+
 def update_radio_group(i: number):
     global radio_group
-    radio_group_max = 0
     radio_group += i
     if radio_group >= radio_group_max:
         radio_group = radio_group_min
     elif radio_group < radio_group_min:
         radio_group = radio_group_max
-    print("radio.set_group(" + ("" + str(radio_group)) + ")")
+    basic.show_number(radio_group)
     radio.set_group(radio_group)
-    basic.show_string("" + str((radio_group)))
+    broadcast_bridge_info()
 # This code convert serial to radio and radio to signal
 
 def on_button_pressed_a():
     update_radio_group(-1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_received_string(receivedString):
+def on_radio_received_string(receivedString):
     serial.write_string(receivedString)
-radio.on_received_string(on_received_string)
+    print ("radio->serial "+receivedString)
+radio.on_received_string(on_radio_received_string)
 
 # This code convert serial to radio
 # 
@@ -27,9 +34,11 @@ def on_button_pressed_b():
     update_radio_group(1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def on_data_received():
-    radio.send_string(serial.read_line())
-serial.on_data_received(serial.delimiters(Delimiters.NEW_LINE), on_data_received)
+def on_serial_data_received():
+    msg=serial.read_line()
+    radio.send_string(msg)
+    print ("serial->radio "+msg)
+serial.on_data_received(serial.delimiters(Delimiters.NEW_LINE), on_serial_data_received)
 
 radio_group_max = 8
 radio_group_min = 0
@@ -37,5 +46,5 @@ radio_group = 0
 serial.redirect_to_usb()
 radio_group = radio_group_min
 radio.set_group(radio_group)
-console.log("Set RadioGroup via button A (inc) and B (dec)")
-console.log("This micro:bit is "+control.device_name())
+broadcast_bridge_info()
+print("Set RadioGroup via button A (inc) and B (dec)")
--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
+function broadcast_bridge_info() {
+    let msg = control.deviceName() + "_" + ("" + control.deviceSerialNumber()) + " is Radio-Serial bridge listening on " + ("" + radio_group)
+    console.log(msg)
+    radio.sendString(msg)
+    serial.writeString(msg)
+}
+
 function update_radio_group(i: number) {
     
-    let radio_group_max = 0
     radio_group += i
     if (radio_group >= radio_group_max) {
         radio_group = radio_group_min
@@ -8,17 +14,18 @@ function update_radio_group(i: number) {
         radio_group = radio_group_max
     }
     
-    console.log("radio.set_group(" + ("" + ("" + radio_group)) + ")")
+    basic.showNumber(radio_group)
     radio.setGroup(radio_group)
-    basic.showString("" + ("" + radio_group))
+    broadcast_bridge_info()
 }
 
 //  This code convert serial to radio and radio to signal
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     update_radio_group(-1)
 })
-radio.onReceivedString(function on_received_string(receivedString: string) {
+radio.onReceivedString(function on_radio_received_string(receivedString: string) {
     serial.writeString(receivedString)
+    console.log("radio->serial " + receivedString)
 })
 //  This code convert serial to radio
 //  
@@ -26,8 +33,10 @@ radio.onReceivedString(function on_received_string(receivedString: string) {
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     update_radio_group(1)
 })
-serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function on_data_received() {
-    radio.sendString(serial.readLine())
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function on_serial_data_received() {
+    let msg = serial.readLine()
+    radio.sendString(msg)
+    console.log("serial->radio " + msg)
 })
 let radio_group_max = 8
 let radio_group_min = 0
@@ -35,5 +44,5 @@ let radio_group = 0
 serial.redirectToUSB()
 radio_group = radio_group_min
 radio.setGroup(radio_group)
+broadcast_bridge_info()
 console.log("Set RadioGroup via button A (inc) and B (dec)")
-console.log("This micro:bit is " + control.deviceName())
